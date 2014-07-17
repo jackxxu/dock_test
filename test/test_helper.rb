0c64d2ac810285117c0128609acb9b1f5e16c6af
Jack Xu <jackxxu@gmail.com>
2014-07-17 19:28:39 -0400

    update the DOCK_ENV switch

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,12 @@ Bundler.require(:default, :test)
 require 'minitest/autorun'
 
 DockTest.config do |c|
-  c.url = (ENV['DOCK_ENV'] == 'production') ? 'http://floating-mesa-6194.herokuapp.com' : 'http://localhost:9871'
+  c.url = case ENV['DOCK_ENV']
+          when 'production'
+            'http://floating-mesa-6194.herokuapp.com'
+          else
+            'http://localhost:9871'
+          end
   c.skippy = :production
 end
 
