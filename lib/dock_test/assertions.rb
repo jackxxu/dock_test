0f7c8ecca5dc8481ded5d5b091fcc4d3e4ead138
Jack Xu <jackxxu@gmail.com>
2014-07-19 12:22:01 -0400

    improve on assertions

--- lib/dock_test/assertions.rb
+++ lib/dock_test/assertions.rb
@@ -1,23 +1,24 @@
 module Minitest::Assertions
   def assert_response_status(status)
-    assert last_response.code == status.to_s, "The response status is expected to be #{last_response.code}"
+    assert last_response.code == status.to_s, "The actual response status is #{last_response.code}"
   end
 
   def assert_response_content_type(content_type)
-    assert last_response['Content-Type'] == content_type.to_s, "The response content_type is expected to be #{last_response['Content-Type']}"
+    assert last_response['Content-Type'] == content_type.to_s, "The actual response content_type is #{last_response['Content-Type']}"
   end
 
   def assert_response_headers(headers, options = {})
     exclude_keys = Array(options[:exclude] || [])
     response_headers = last_response.to_hash.delete_if {|k, _| exclude_keys.include?(k)}
+    assert response_headers == headers, "The actual response headers are #{response_headers.inspect}"
   end
 
   def assert_response_body(body_string)
-    assert last_response.body == body_string, "The response body is expected to be #{last_response.body}"
+    assert last_response.body == body_string, "The actual response body is #{last_response.body}"
   end
 
   def assert_response_json_schema(schema_path)
     schema = File.open(schema_path).read
-    assert JSON::Validator.validate(schema, last_response.body), "The response is expected to have a schema defined in #{schema_path}"
+    assert JSON::Validator.validate(schema, last_response.body), "The actual response does not match the schema defined in #{schema_path}"
   end
 end
